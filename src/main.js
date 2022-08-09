f743b52a3a3428fca7594b4db7181787d9abad47
李晓宁 <xiaoning.li@inossem.com>
2022-08-09 11:58:02 +0800

    feat: 测试多环境打包配置

--- src/main.js
+++ src/main.js
@@ -2,17 +2,14 @@
  * @Author: xiaoning.li@inossem.com
  * @Date: 2022-07-27 10:21:15
  * @LastEditors: xiaoning.li@inossem.com
- * @LastEditTime: 2022-07-29 11:54:29
+ * @LastEditTime: 2022-08-09 11:48:47
  * @Description: ToolsJs入口文件 我随便
  */
 import * as typeCheck from './typeCheck'
 import * as validate from './validate'
+
 const toolsJs = {
     typeCheck,
     validate,
-    name: {
-        // name: 123,
-        // age: 123,
-    },
 }
 export default toolsJs
