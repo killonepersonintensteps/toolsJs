f19055af7c6fd7ef257f205bf6727518771275af
李晓宁 <xiaoning.li@inossem.com>
2022-08-30 10:07:35 +0800

    fix: 使用 unkonwn 代替 any

--- src/typeCheck.ts
+++ src/typeCheck.ts
@@ -2,22 +2,23 @@
  * @Author: xiaoning.li@inossem.com
  * @Date: 2022-07-27 16:55:50
  * @LastEditors: xiaoning.li@inossem.com
- * @LastEditTime: 2022-08-29 17:48:58
+ * @LastEditTime: 2022-08-30 10:06:19
  * @Description: 数据类型检查
  */
 
-export const isArray = (v: any) => Object.prototype.toString.call(v) === '[object Array]'
+export const isArray = (v: unknown) => Object.prototype.toString.call(v) === '[object Array]'
 
-export const isObject = (v: any) => Object.prototype.toString.call(v) === '[object Object]'
+export const isObject = (v: unknown) => Object.prototype.toString.call(v) === '[object Object]'
 
-export const isString = (v: any) => Object.prototype.toString.call(v) === '[object String]'
+export const isString = (v: unknown) => Object.prototype.toString.call(v) === '[object String]'
 
-export const isNumber = (v: any) => Object.prototype.toString.call(v) === '[object Number]'
+export const isNumber = (v: unknown) => Object.prototype.toString.call(v) === '[object Number]'
 
-export const isBoolean = (v: any) => Object.prototype.toString.call(v) === '[object Boolean]'
+export const isBoolean = (v: unknown) => Object.prototype.toString.call(v) === '[object Boolean]'
 
-export const isFunction = (v: any) => Object.prototype.toString.call(v) === '[object Function]'
+export const isFunction = (v: unknown) => Object.prototype.toString.call(v) === '[object Function]'
 
-export const isUndefined = (v: any) => Object.prototype.toString.call(v) === '[object Undefined]'
+export const isUndefined = (v: unknown) =>
+  Object.prototype.toString.call(v) === '[object Undefined]'
 
-export const isNull = (v: any) => Object.prototype.toString.call(v) === '[object Null]'
+export const isNull = (v: unknown) => Object.prototype.toString.call(v) === '[object Null]'
